21339f8eeae67483c1c5680258112a2b20056033
Alex <thealex11011999@gmail.com>
2019-09-27 14:28:54 +0200

    nuevo archivo:  LeerPersona.js 	nuevo archivo:  Persona.js 	nuevo archivo:  Programa.js 	nuevo archivo:  prueba.csv

---
+++ LeerPersona.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const Persona = require('./Persona')
+
+//console.log(process.argv)
+
+if (!process.argv[2]) {
+  console.log('Falta argumento...')
+  process.exit(1)
+}
+
+let ruta = process.argv[2]
+
+fs.readFile(ruta, 'utf-8', (err, data) => {
+  if (err) {
+    console.log('No existe el archivo')
+    process.exit(2)
+  }
+  console.log(data)
+})
---
+++ Persona.js
@@ -0,0 +1,45 @@
+module.exports = class Persona {
+
+  constructor(nombre, apellido, email, sexo) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.email = email
+    this.sexo = sexo
+  }
+
+  get nombre() {
+    return this.nombre
+  }
+
+  set nombre(value) {
+    this.nombre = value
+  }
+
+  get apellido() {
+    return this.apellido
+  }
+
+  set apellido(value) {
+    this.apellido = value
+  }
+
+  get email() {
+    return this.email
+  }
+
+  set email(value) {
+    this.email = value
+  }
+
+  get sexo() {
+    return this.sexo
+  }
+
+  set sexo(value) {
+    this.sexo = value
+  }
+
+  toString() {
+    return "Nombre: " + this.nombre + "Apellido: " + this.apellido + "email:" + this.email + "sexo: " + this.sexo
+  }
+}
