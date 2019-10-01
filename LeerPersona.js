8e58d2399e831f99c26f9cab19d2fa5d32ea2261
Alex <thealex11011999@gmail.com>
2019-10-01 14:12:35 +0200

    modificado:     LeerPersona.js

--- LeerPersona.js
+++ LeerPersona.js
@@ -1,5 +1,9 @@
+//A ver mete todo dentro de la funcion de lectura del fichero y al añadir las personas crealas en el bucle
+//no crees una externa y luego rellenes
+
+
 const fs = require('fs')
-const Persona = require('./Persona')
+const Persona = require('./Persona.js')
 
 //console.log(process.argv)
 
@@ -10,31 +14,41 @@ if (!process.argv[2]) {
 
 let ruta = process.argv[2]
 let datos
+let lineasCSV
+let personaCSV
+let listaPersonas
+
 
 fs.readFile(ruta, 'utf-8', (err, data) => {
   if (err) {
     console.log('No existe el archivo')
-    datos = data
     process.exit(2)
   }
-  console.log(data)
+  datos = data  
+  separarPersonas()
 })
 
-let lineasCSV = datos.split("\n")
-let personaCSV
-let listaPersonas
-let persona = new Persona()
+function separarPersonas() {
+  lineasCSV = datos.split('\n')
+  console.log(typeof(lineasCSV))
+  //let persona = new Persona()
+  // for (let i=1; i < lineasCSV.size()-1; i++) {
+  //   personaCSV = lineasCSV[i].split(',')
 
-for (let i=1; i < lineasCSV.size()-1; i++) {
-  personaCSV = lineasCSV[i].split(",")
-  persona.nombre = personaCSV[0]
-  persona.apellido = personaCSV[1]
-  persona.email = personaCSV[2]
-  persona.sexo = personaCSV[3]
-
-  listaPersonas.push(persona)
+    // persona.nombre = personaCSV[0]
+    // persona.apellido = personaCSV[1]
+    // persona.email = personaCSV[2]
+    // persona.sexo = personaCSV[3]
+  
+    // listaPersonas.push(persona)
+  //}
+  
+  // for (let i=0; i < listaPersonas.size(); i++){
+  //   console.log(listaPersonas[i])
+  // }
 }
 
-for (let i=0; i < listaPersonas.size(); i++){
-  console.log(listaPersonas[i])
-}
+
+
+
+
